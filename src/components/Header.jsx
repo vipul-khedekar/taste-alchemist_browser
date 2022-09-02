093d9d95e58166598d39c8b93866e2607831d478
vipul-khedekar <3kvipul@gmail.com>
2022-09-02 17:51:54 +0530

    Fix: Linked logo to home.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 import Logo from "../images/logo.png";
 import { HeaderContainer } from "../styles/StyledHeader";
 
 function Header() {
   return (
-    <HeaderContainer>
-      <img src={Logo} alt="logo" />
-      <p>TasteAlchemist</p>
-    </HeaderContainer>
+    <Link to={`/`} style={{ textDecoration: `none` }}>
+      <HeaderContainer>
+        <img src={Logo} alt="logo" />
+        <p>TasteAlchemist</p>
+      </HeaderContainer>
+    </Link>
   );
 }
 
